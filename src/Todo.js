b8fffb7a49a7cac3cda92d6a6640b03dd4ca3a97
hina81 <i20203@e.hiroshima-cu.ac.jp>
2024-05-25 11:16:00 +0900

    [start]Todoリストの作成

---
+++ src/Todo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Todo = ({ todo, toggleTodo }) => {
+    const handleTodoclick = () => {
+        toggleTodo(todo.id)
+    }
+
+    return (
+        <div>
+            <label>
+                <input type="checkbox"
+                    checked={todo.completed}
+                    readOnly
+                    onChange={handleTodoclick} />
+            </label>
+            {todo.name}
+        </div>
+    )
+}
+
+export default Todo
